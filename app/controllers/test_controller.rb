0a81ca681c199f4d7e6b4e36751652b712e16ccc
rthroop <rthroop@cloverhound.com>
2016-04-16 16:02:51 -0400

    ask test example

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -12,6 +12,16 @@ def index
 	t.say(:value => "Thanks for calling the Charlotte-Mecklenburg Parks Department Weather Hotline!")
 	render :json => t.response
 
+	t = Tropo::Generator.new
+	t.ask :name => 'color',
+
+				 :timeout => 60,
+
+				 :say => {:value => "What's your favorite color?  Choose from red, blue or green."},
+
+				 :choices => {:value => "red, blue, green"}
+	t.on :event => 'continue', :next => '/continue.json'
+	t.response
 end
 
 end
