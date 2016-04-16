0c329b0c3749726047e3d20c09cb09572b64dc21
rthroop <rthroop@cloverhound.com>
2016-04-16 13:14:58 -0400

    CM Parks Weather Hotline

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,3 +1,6 @@
+
+
+
 class TestController < ApplicationController
 
 skip_before_action :verify_authenticity_token
@@ -5,11 +8,9 @@ skip_before_action :verify_authenticity_token
 def index
 
 
-t = Tropo::Generator.new
-t.say(:value => "Hello World!")
+	t = Tropo::Generator.new
+	t.say(:value => "Thanks for calling the Charlotte-Mecklenburg Parks Department Weather Hotline!")
 
-render :json => t.response
 end
-
 
 end
