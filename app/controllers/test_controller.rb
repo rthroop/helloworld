9f8b5fde810795a5419cdc18ba0a36bd3d5048c8
rthroop <rthroop@cloverhound.com>
2016-04-16 16:05:00 -0400

    ask test example

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -8,9 +8,9 @@ skip_before_action :verify_authenticity_token
 def index
 
 
-	t = Tropo::Generator.new
-	t.say(:value => "Thanks for calling the Charlotte-Mecklenburg Parks Department Weather Hotline!")
-	render :json => t.response
+	#t = Tropo::Generator.new
+	#t.say(:value => "Thanks for calling the Charlotte-Mecklenburg Parks Department Weather Hotline!")
+	#render :json => t.response
 
 	t = Tropo::Generator.new
 	t.ask :name => 'color',
